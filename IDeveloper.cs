930e52e48fa541a24206e9562a5e130db7bafa3c
romaterekh <terekhroman2002@gmail.com>
2022-01-13 16:48:12 +0200

    Добавьте файлы проекта.

---
+++ IDeveloper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H5
+{
+    interface IDeveloper : IComparable<IDeveloper>
+    {
+        string Tool
+        { get; }
+        void Create();
+        void Destroy();
+    }
+}
